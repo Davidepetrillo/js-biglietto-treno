334de53feb17ee972ec2b8064f7eb7143ae2a353
davidepetrillo <davide96petrillo@gmail.com>
2022-03-22 17:38:01 +0100

    commit 5 - modificato bonus 2 (finito)

--- bonus2.js
+++ bonus2.js
@@ -1,32 +1,20 @@
+
 const primaScelta = prompt("scegli pari o dispari");
-primaScelta = primaScelta.toLowerCase();
-primaScelta = primaScelta.replace(" ", "");
+const sceltaGiocatore = prompt('Inserisci un numero compreso tra 1 e 9 che intendi giocare');
+const scelta1 = parseInt(sceltaGiocatore)
+console.log(scelta1)
+const sceltaComputer = Math.floor(Math.random()*10);
+const scelta2 = parseInt(sceltaComputer)
+console.log (scelta2)
+const somma = scelta1 + scelta2;
+console.log(somma);
 
-
-if (primaScelta == "pari") {
-    const secondaSceltaPari = prompt("Inserisci un numero pari compreso tra 1 e 9 che intendi giocare");
-    if (secondaSceltaPari % 2 != 0) {
-        alert('La prego di scegliere un numero pari');
-    } else if (secondaSceltaPari %2 == 0) {
-    const randInt = Math.floor(Math.random()*10);
-    if ( randInt %2 == 0) {
-        alert('Hai vinto questo round');
-    } else {
-        alert('Hai perso questo round');
-    }
-}
-}
-
-else if (primaScelta == "dispari") {
-    const secondaSceltaDispari = prompt("Inserisci un numero dispari compreso tra 1 e 9 che intendi giocare");
-    if (secondaSceltaDispari % 2 == 0) {
-        alert('La prego di scegliere un numero dispari');
-} else if (secondaSceltaDispari %2 == 0) {
-    const randInt2 = Math.floor(Math.random()*10);
-    if ( randInt2 %2 == 0) {
-        alert('Hai vinto questo round');
-    } else {
-        alert('Hai perso questo round');
-    }
-}
+if ((primaScelta == "pari") && (somma % 2 == 0)) {
+    alert('Hai vinto questo round');
+} else if ((primaScelta == "pari") && (somma % 2 != 0)) {
+    alert('Hai perso questo round');
+} else if ((primaScelta == "dispari") && (somma % 2 == 0)) {
+    alert('Hai perso questo round');
+} else if ((primaScelta == "dispari") && (somma % 2 != 0)) {
+    alert('Hai vinto questo round');
 }
